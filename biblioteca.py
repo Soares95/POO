717c93ec4aac0c09db1b45f90c21fddd6b4a835c
Diego <munizmsb001@gmail.com>
2025-05-07 11:36:39 -0300

    POO01

---
+++ biblioteca.py
@@ -0,0 +1,20 @@
+class Pessoa():
+    def __init__(self, nome, idade, peso):
+        self.nome=nome
+        self.peso=peso
+        self.idade=idade
+
+    def comer(self, comida):
+        print(f"comer {comida}")
+    def durmir(self, tarde):
+        print(f"durmir {tarde}")
+    def falar(self, poesia):
+        print(f"falar {poesia}")
+
+    def comer(self):
+        if not self.durmindo and not self.falando
+            self.comendo=True
+            print(f"ele esta {comendo}")
+        elif self.durmindo:
+            print
+
---
+++ main.py
@@ -0,0 +1,16 @@
+from biblioteca import Pessoa
+
+aluno01=Pessoa("Diego",29, 84)
+aluno02=Pessoa("caleb",25, 75)
+aluno03=Pessoa("Ruy",24, 78)
+print(aluno01.nome)
+print(aluno01.idade)
+print(aluno01.peso)
+
+aluno01.comer("pão")
+aluno01.durmir("tarde")
+aluno01.falar("poesia")
+
+
+
+
