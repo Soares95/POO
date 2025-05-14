61829dfccaa9bd0efa483e2e90540f91e8062211
Diego <munizmsb001@gmail.com>
2025-05-14 12:05:50 -0300

    POO01 atualizado

--- biblioteca.py
+++ biblioteca.py
@@ -1,8 +1,14 @@
+from symtable import Class
+
+
 class Pessoa():
     def __init__(self, nome, idade, peso):
         self.nome=nome
         self.peso=peso
         self.idade=idade
+        self.comer=False
+        self.falar=False
+        self.durmir=False
 
     def comer(self, comida):
         print(f"comer {comida}")
@@ -12,9 +18,76 @@ class Pessoa():
         print(f"falar {poesia}")
 
     def comer(self):
-        if not self.durmindo and not self.falando
+        if not self.durmindo and not self.falando:
             self.comendo=True
-            print(f"ele esta {comendo}")
+            print(f"ele esta ")
         elif self.durmindo:
             print
 
+class Animal():
+    def __init__(self, nome, cor):
+        self.nome=nome
+        self.cor=cor
+    def comer(self):
+        print(f"O {self.nome} foi comer...")
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def miar(self):
+        print(f" o {self.nome} foi miando...")
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def latir(self):
+        print(f" o {self.nome} corre atrás de mosca e late...")
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def cararejar(self):
+        print(f" o {self.nome} foi cacarejou...")
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome,cor)
+    def mugir(self):
+        print(f" o {self.nome} mugiu...")
+
+    def comer(self):
+        print(f"O {self.nome} foi comer capim...")
+
+class Ingresso():
+    def __init__(self, valor):
+        self.valor = valor
+        super().__init__(valor)
+    def imprimeValor(self):
+        print(f" o ingresso custou {self.valor:.2f} ")
+class Vip():
+    def __init__(self, valor):
+        super().__init__(valor*1.5)
+    def imprimeValor(self):
+        print(f" o ingresso VIP custou {self.valor:.2f} ")
+
+class Forma:
+    def __init__(self):
+        self.area=0
+        self.perimetro=0
+
+class Triangulo:
+    def __init__(self, altura):
+        super().__init__(altura)
+    def calcularArea(self):
+    def calcularPerimetro(self):
+
+class Retangulo(Forma):
+    def __init__(self):
+        super().__init__()
+    def calcularArea(selfself,base,altura):
+        self.area=base*altura
+        print(f"A Area do retangulo é {self.area}")
+    def calcularPerimetro(self, base, altura):
+        self.perimetro=(base+altura)*2
+        print(f"O perimetro do retangulo é {self.perimetro} ")
+        
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from biblioteca import Pessoa
+from biblioteca import Pessoa, Cachorro, Gato, Coelho, Vaca, Animal, Ingresso
 
 aluno01=Pessoa("Diego",29, 84)
 aluno02=Pessoa("caleb",25, 75)
 aluno03=Pessoa("Ruy",24, 78)
-print(aluno01.nome)
+'''print(aluno01.nome)
 print(aluno01.idade)
-print(aluno01.peso)
+print(aluno01.peso)'''
 
-aluno01.comer("pão")
-aluno01.durmir("tarde")
-aluno01.falar("poesia")
+Mascote=Animal("Milo","branca")
+Mascote.comer()
 
+bixo=Gato("Milo","branca")
+bixo.miar()
 
-
-
+cinema=Ingresso(20)
+cinema.imprimeValor()
+cineVip=Vip(20)
+cineVip.imprimeValor()
